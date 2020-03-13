186d71eec3b48a032ab44f9e1c085d940c9f4b02
HarrisMBrad <bmichaelh13@gmail.com>
2020-03-13 19:29:13 -0400

    Use this to convert meters to inches

---
+++ app/src/main/java/com/gmail/meterstoinches/bmichaelh13/MainActivity.java
@@ -0,0 +1,57 @@
+package com.gmail.meterstoinches.bmichaelh13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+        1m = 39.3701in
+    */
+
+    private EditText enterMeters;
+    private Button convertButton;
+    private TextView resultTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enterMeters = (EditText) findViewById(R.id.metersEditTextId);
+        resultTextView = (TextView) findViewById(R.id.resultId);
+
+        convertButton = (Button) findViewById(R.id.convertButtonId);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Conversion logic
+                double multiplier = 39.37;
+                double result = 0.0;
+
+                if (enterMeters.getText().toString().equals("")) {
+
+                    resultTextView.setText(R.string.error_message);
+                    resultTextView.setTextColor(Color.RED);
+
+                }else {
+                    double meterValue = Double.parseDouble(enterMeters.getText().toString()) ;
+                    result = meterValue*multiplier;
+
+                    resultTextView.setTextColor(Color.DKGRAY);
+                    resultTextView.setText(String.format("%.2f", result) + " inches " );
+                }
+
+
+            }
+        });
+
+
+    }
+}
